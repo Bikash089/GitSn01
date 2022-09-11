2d418411f7d81436f031381e7839504063254023
Bikash Maiti <bukashnsec@gmail.com>
2022-09-11 17:02:45 +0530

    addng sum.java

---
+++ sum.java
@@ -0,0 +1,28 @@
+
+
+public class PrimeNum {
+
+	public static void main(String[] args) {
+		
+
+		int num = 56243;
+
+		boolean flag = false;
+		
+		for (int i=2; i<=num/2; i++)
+		{
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		
+		if(!flag) {
+			System.out.println("The number is PRIME Num");
+		}
+		else {
+			System.out.println("The number is not PRIME");
+		}
+	}
+
+}
