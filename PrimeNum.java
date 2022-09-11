f25a64a695ca16d54a5413fa3f4e9e0a81b6b663
Bikash Maiti <bukashnsec@gmail.com>
2022-09-11 16:43:33 +0530

    initial commit

---
+++ PrimeNum.java
@@ -0,0 +1,28 @@
+
+
+public class PrimeNum {
+
+	public static void main(String[] args) {
+		
+
+		int num = 5624;
+
+		boolean flag = false;
+		
+		for (int i=2; i<=num/2; i++)
+		{
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		
+		if(!flag) {
+			System.out.println("The number is PRIME");
+		}
+		else {
+			System.out.println("The number is not PRIME");
+		}
+	}
+
+}
